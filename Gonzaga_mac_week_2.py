0a194bba0bbb84443228ff5a60b7720a0d652d5d
MaxGonzaga1 <gonzaga.macjoxelle12282006@gmail.com>
2026-02-13 09:55:45 +0800

    Add initial implementation of score evaluation and personal information display

---
+++ Gonzaga_mac_week_2.py
@@ -0,0 +1,16 @@
+name = "Mac Joxelle S. Gonzaga"
+age = 19
+height = 1.67
+work = True
+
+print(f"Fullname: {name}")
+print(type(name))
+
+print(f"Age: {age}")
+print(type(age))
+
+print(f"Hight in meters: {height}")
+print(type(height))
+
+print(f"Currently working? {work}")
+print(type(work))
