91f604d78d0a18be94bbb23b934be5037a760b03
MaxGonzaga1 <gonzaga.macjoxelle12282006@gmail.com>
2026-03-10 10:48:19 +0800

    Create userInput.py

---
+++ userInput.py
@@ -0,0 +1,4 @@
+name = input("Enter full name: ")
+age = int(input("Enter age: "))
+address = input("Enter address: ")
+print(f" Hello! I am {name}, {age} years old and currently living at {address}")
